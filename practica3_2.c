9014ab0c17d48567f3f2e3684ca7972a04b741d1
Lucia <1668160@uab.cat>
2022-11-24 17:24:01 +0100

    ejercicio 3

---
+++ practica3_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  short i,j;
+  i=1;
+  j=2;
+  i++;
+  j+=i;
+  return 0;
+}
---
+++ practica3_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  int a=0,b=1,c=2,d=3;
+  a=b+c*d;
+  a=(b+c)*d;
+  d=a;
+  return 0;
+}
